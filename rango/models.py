be3a265d84cf9cf260b664a4e7840d39825cdc15
wanzifa <773857626@qq.com>
2016-02-16 20:06:26 +0800

    update unit 7

--- rango/models.py
+++ rango/models.py
@@ -1,14 +1,21 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.template.defaultfilters import slugify
 
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
     views = models.IntegerField(default=0)
     likes = models.IntegerField(default=0)
+    slug = models.SlugField()
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Category, self).save(*args, **kwargs)
 
     class Meta:
         verbose_name_plural = "categories"
+
     def __unicode__(self):
         return self.name
 
--- rango/views.py
+++ rango/views.py
@@ -1,11 +1,32 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from models import Category, Page
 
 
 def index(request):
-    context_dict = {'boldmessge': "I am bold font from the context"}
-
+    context_dict = {}
+    category_list = Category.objects.order_by('-likes')[:5]
+    context_dict['categories'] = category_list
+    page_list = Page.objects.order_by('-views')[:5]
+    context_dict['pages'] = page_list
     return render(request, "rango/index.html", context_dict)
 
 def about(request):
     return render(request, "rango/about.html")
+
+def category(request, category_name_slug):
+    context_dict = {}
+
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+        context_dict['category_name'] = category.name
+
+        pages = Page.objects.filter(category=category)
+
+        context_dict['pages'] = pages
+        context_dict['category'] = category
+
+    except Category.DoesNotExist:
+        pass
+
+    return render(request, 'rango/category.html', context_dict)
