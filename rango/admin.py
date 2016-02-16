cee9ab8415f6ac829858e6329415e22f7bf61d9d
wanzifa <773857626@qq.com>
2016-02-16 11:12:51 +0800

    update

--- rango/admin.py
+++ rango/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from rango.models import Category, Page
 
-# Register your models here.
+
+class CategoryAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields':['name']}),
+        ('Other information', {'fields': ['likes', 'views'],
+         'classes': ['collapse']})
+        ]
+
+
+class PageAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'url')
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Page, PageAdmin)
--- rango/models.py
+++ rango/models.py
@@ -2,4 +2,23 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=128, unique=True)
+    views = models.IntegerField(default=0)
+    likes = models.IntegerField(default=0)
+
+    class Meta:
+        verbose_name_plural = "categories"
+    def __unicode__(self):
+        return self.name
+
+
+class Page(models.Model):
+    category = models.ForeignKey(Category)
+    title = models.CharField(max_length=128)
+    url = models.URLField()
+    views = models.IntegerField(default=0)
+
+    def __unicode__(self):
+        return self.title
+
